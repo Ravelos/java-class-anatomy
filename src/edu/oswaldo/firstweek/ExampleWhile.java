abebcffd8824eae8676658349f14068003f6ff4a
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-29 17:59:32 -0300

    Gives an example for while

--- src/edu/oswaldo/firstweek/ExampleWhile.java
+++ src/edu/oswaldo/firstweek/ExampleWhile.java
@@ -1,2 +1,30 @@
-package edu.oswaldo.firstweek;public class ExampleWhile {
+package edu.oswaldo.firstweek;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExampleWhile {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+
+        while(mesada > 0) {
+            double valorDoce = valorAleatorio();
+
+            if(valorDoce > mesada)
+                valorDoce = mesada;
+
+            System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho");
+            mesada = mesada - valorDoce;
+        }
+        System.out.println("Mesada:" + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces");
+
+        /*
+         * Não se preocupe quanto a formatação de valores
+         * Iremos explorar os recursos de formatação em breve !!
+         */
+    }
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+
 }
