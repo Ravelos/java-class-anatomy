deee7b9c48b6295213ed7870c0bb41e14186c5cb
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-29 17:59:06 -0300

    Gives an example for do while

--- src/edu/oswaldo/firstweek/ExampleDoWhile.java
+++ src/edu/oswaldo/firstweek/ExampleDoWhile.java
@@ -1,2 +1,23 @@
-package edu.oswaldo.firstweek;public class ExampleDoWhile {
+package edu.oswaldo.firstweek;
+
+import java.util.Random;
+
+public class ExampleDoWhile {
+    public static void main(String[] args) {
+            System.out.println("Discando...");
+
+            do {
+                //excutando repetidas vezes até alguém atender
+                System.out.println("Telefone tocando");
+
+            }while(tocando());
+
+            System.out.println("Alô !!!");
+        }
+        private static boolean tocando() {
+            boolean atendeu = new Random().nextInt(3)==1;
+            System.out.println("Atendeu? " + atendeu);
+            //negando o ato de continuar tocando
+            return ! atendeu;
+        }
 }
