43b25e461685b69ea0b4c8e66de810dac650777e
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-29 18:03:10 -0300

    Gives an example for using arrays

---
+++ src/edu/oswaldo/firstweek/ExampleForArray.java
@@ -0,0 +1,11 @@
+package edu.oswaldo.firstweek;
+
+public class ExampleForArray {
+    public static void main(String[] args) {
+         String[] alunos = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+         for(int x=0; x< alunos.length; x++){
+             System.out.println("O aluno no indice x=" + x+" é " + alunos[x]);
+         }
+    }
+}
