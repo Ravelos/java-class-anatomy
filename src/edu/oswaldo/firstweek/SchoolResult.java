3a3c9346235b233e5a6b70d41c53ed21cd2751ac
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-29 18:04:15 -0300

    Modifies school result class

---
+++ src/edu/oswaldo/firstweek/SchoolResult.java
@@ -0,0 +1,9 @@
+package edu.oswaldo.firstweek;
+
+public class SchoolResult {
+    public static void main(String[] args) {
+        int grade = 4;
+        String result = grade >= 7 ? "Approved" : "Fail";
+        System.out.println(result);
+    }
+}
