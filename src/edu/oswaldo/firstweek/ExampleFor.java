f60ecfa59a175586710c329623e348f5493db27a
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-29 18:02:40 -0300

    Gives an example for using for

---
+++ src/edu/oswaldo/firstweek/ExampleFor.java
@@ -0,0 +1,10 @@
+package edu.oswaldo.firstweek;
+
+public class ExampleFor {
+    public static void main(String[] args) {
+        for(int lambs = 1 ;lambs <= 20; lambs++ ){
+            System.out.println("Counting lambs" + lambs);
+        }
+        System.out.println("He is slept");
+    }
+}
